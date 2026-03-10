163c12366fb74dd947b724d3c2141ee00b28e399
alanis07 <151046540+alanis07@users.noreply.github.com>
2026-03-10 08:25:18 -0300

    Add files via upload

---
+++ Atividade2.py
@@ -0,0 +1,11 @@
+idade=input("digite sua idade:")
+idade=int(idade)
+maioridade=idade>=18
+menoridade=idade<18
+
+if maioridade:
+    print("o usuáro é maior de idade")
+
+else:
+    print("o usuário é menor de idade")
+
