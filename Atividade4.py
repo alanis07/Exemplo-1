6ac6b535a2e18793144c24569c44ef8dabbb5621
alanis07 <151046540+alanis07@users.noreply.github.com>
2026-03-10 08:57:55 -0300

    Add files via upload

--- Atividade4.py
+++ Atividade4.py
@@ -4,7 +4,7 @@ notalta=nota>=6.0
 notabaixa=nota<6.0
 
 if notalta:
-    print("Anota do aluno:{nota} aluno aprovado")
+    print(f"A nota do aluno:{nota} aluno aprovado")
 
 else:
-    print("A nota do aluno: {nota} aluno reprovdo")
+    print(f"A nota do aluno: {nota} aluno reprovado")
